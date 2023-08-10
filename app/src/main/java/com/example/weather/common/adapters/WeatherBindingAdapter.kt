3d051826fecccd3a572eb48991e6d099fe62e413
Geeckmaster69 <emmanuelmosa@gmail.com>
2023-08-09 19:00:38 -0600

    First commit

---
+++ app/src/main/java/com/example/weather/common/adapters/WeatherBindingAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.weather.common.adapters
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+
+
+    @BindingAdapter("isGone")
+    fun bindIsGone(view: View, isGone: Boolean){
+        view.visibility = if (isGone) View.GONE else View.VISIBLE
+    }
